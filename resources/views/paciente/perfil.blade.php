7e16d6d77ec446f6e1ea42519f784f1155414acb
Daniela <daniela.milon@ucsp.edu.pe>
2018-06-01 20:59:53 -0500

    Usuario perfil

--- resources/views/paciente/perfil.blade.php
+++ resources/views/paciente/perfil.blade.php
@@ -2,5 +2,179 @@
 @section('content')
 
 
+<section class="we-offer-area section-gap" id="offer">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-60 col-lg-10">
+            	<div class="single-testimonial item">
+                    <img class="mx-auto" src="img/cliente_1.png" alt="">
+                    <p class="desc">
+                        En esta sección se encuentra información relevante del paciente: Nombre, Edad, Enfermedades hereditarias, alergias, etc. La información que coloque deberá ser objetiva y precisa.
+                    </p>
+                    <h4>Luke Skywalker</h4>
+                    <p>
+                        Abogado de Cementos Yura
+                    </p>
+                </div>
+                <div class="title text-center">
+                    <h1 class="mb-10">Datos Personales</h1>
+                    <p>Información de Contacto.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="single-offer d-flex flex-row pb-30">
+                    <div class="icon">
+                        <img src="img/contacto.png" alt="">
+                    </div>
+                    <div class="desc">
+                         <a href="#"><h5>Nombre Completo</h5></a>
+                        <p>
+                            Luke Skywaker
+                        </p>
+                        <a href="#"><h5>Tipo de Sangre</h5></a>
+                        <p>
+                            A+
+                        </p>
+                        <a href="#"><h5>Sexo</h5></a>
+                        <p>
+                            Masculino
+                        </p>
+                    </div>
+                </div>
+                <div class="single-offer d-flex flex-row pb-30">
+                    <div class="icon">
+                        <img src="img/s4.png" alt="">
+                    </div>
+                    <div class="desc">
+                        <a href="#"><h4>Padecimientos Hereditarios</h4></a>
+                        <p>    <b>Madre:</b> Mi madre sufre de diabetes Tipo 2.</p>
+                        <p>    <b>Padre:</b> Mi padre sufre de hipertención.</p>
+                        <p>    <b>Alergias:</b> No poseo alergia a nada.</p>
+                        <p>    <b>Mis padecimientos:</b> Sufre de diabetes Tipo 2, tengo el colesterol alto. </p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="single-offer d-flex flex-row pb-30">
+                    <div class="icon">
+                        <img src="img/phone.jpg" alt="">
+                    </div>
+                    <div class="desc">
+                        <a href="#"><h5>Correo Electrónico</h5></a>
+                        <p>
+                            luke.sky@starwars.com
+                        </p>
+                        <a href="#"><h5>Número Telefónico</h5></a>
+                        <p>
+                            +51 99855233
+                        </p>
+                        <a href="#"><h5>Número Telefónico en caso de Emergencia</h5></a>
+                        <p>
+                            +51 985774236
+                        </p>
+                    </div>
+                </div>
+                <div class="single-offer d-flex flex-row pb-30">
+                    <div class="icon">
+                        <img src="img/ambulance.png" alt="">
+                    </div>
+                    <div class="desc">
+                        <a href="#"><h4>Intervenciones Quirúrguicas</h4></a>
+                        <p>
+                            Operación de apéndice.
+                        </p>
+                        <p>
+                            Inciciones dentales.
+                        </p>
+                        <p>
+                            Operación de meñiscos.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-60 col-lg-10">
+            	
+                <div class="title text-center">
+                    <h1 class="mb-10">Razón Social de Seguro Asociado</h1>
+                    <p>Descripción de los datos de la afiliación médica.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="single-offer d-flex flex-row pb-30">
+                    <div class="icon">
+                        <img src="img/hospital.png" alt="">
+                    </div>
+                    <div class="desc">
+                        <a href="#"><h4>Lugar de Residencia Actual</h4></a>
+                        <p>
+                            Arequipa - Perú
+                        </p>
+                        <a href="#"><h4>Clínica Afiliada</h4></a>
+                        <p>
+                            Clínica Arequipa
+                        </p>
+                        <a href="#"><h4>Beneficios Médicos</h4></a>
+                        <p>
+                            El seguro cubre a toda mi familia
+                        </p>
+                    </div>
+                </div>
+                <div class="single-offer d-flex flex-row pb-30">
+                    <div class="icon">
+                        <img src="img/house.png" alt="">
+                    </div>
+                    <div class="desc">
+                        <a href="#"><h4>Familia</h4></a>
+                        <p>
+                            <b>Estado Civil: </b>Casado
+                        </p>
+                        <p>
+                            <b>Hijos: </b>2mujeres y un varón
+                        </p>
+                        <p>
+                            <b>Estado Económico: </b>Básico-Medio
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!--
+            <div class="col-lg-6">
+                <div class="single-offer d-flex flex-row pb-30">
+                    <div class="icon">
+                        <img src="img/s2.png" alt="">
+                    </div>
+                    <div class="desc">
+                        <a href="#"><h4>Smart Security</h4></a>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.
+                        </p>
+                    </div>
+                </div>
+            
+                <div class="single-offer d-flex flex-row pb-30">
+                    <div class="icon">
+                        <img src="img/s4.png" alt="">
+                    </div>
+                    <div class="desc">
+                        <a href="#"><h4>Smart Security</h4></a>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.
+                        </p>
+                    </div>
+                </div>-->
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End we-offer Area -->
 
 @endsection
