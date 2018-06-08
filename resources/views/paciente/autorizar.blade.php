2a53ec4f5d35f6b6ba38a52f234642c58e591505
ltch23 <luistong23@gmail.com>
2018-06-07 22:21:59 -0500

    agregando vista de autorizar

--- resources/views/paciente/autorizar.blade.php
+++ resources/views/paciente/autorizar.blade.php
@@ -5,7 +5,20 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
+                <h5>Mis Doctores</h5>
+                                        
+                    <ul>
+                    <li><a href="mMedico1">Juan Perez</a></li>
+                    <li><a href="#medico2">Jose Zuñiga</a></li>
+                    <li><a href="#medico3">Luis Barios</a></li>
+                    </ul>
                 <h5>Autorizar</h5>
+    
+                <form id="buscador" name="buscador" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>"> 
+                    <input id="buscar" name="buscar" type="search" placeholder="Ingrese nombre de medico aqui..." autofocus >
+                    <input type="submit" name="buscador" class="boton peque aceptar" value="buscar">
+                </form>
+                            
             </div>
         </div>
     </div>
