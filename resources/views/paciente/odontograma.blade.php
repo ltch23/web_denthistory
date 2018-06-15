a1e8a56b3b91f44f774848d4cb81caabd8b84d6b
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2018-06-15 16:26:52 -0500

    agrego vista del diente en modal

--- resources/views/paciente/odontograma.blade.php
+++ resources/views/paciente/odontograma.blade.php
@@ -15,11 +15,11 @@
                         <td>
                             <div class="diente" data-diente="diente{{$i}}" data-name="{{$dientes[$i-1]}}">
                                 <div class="dent{{$i}}">
-                                    <div class="trapecio-a"></div>
-                                    <div class="trapecio-b"></div>
+                                    <div class="trapecio-1"></div>
+                                    <div class="trapecio-3"></div>
                                     <div class="cuadrado"></div>
-                                    <div class="trapecio-i"></div>
-                                    <div class="trapecio-d"></div>
+                                    <div class="trapecio-4"></div>
+                                    <div class="trapecio-2"></div>
                                     <div class="cross" style="background-color: transparent"></div>
                                     <div class="cross2" style="background-color: transparent"></div>
                                     <div class="puente" style="background-color: transparent"></div>
@@ -34,11 +34,11 @@
                         <td>
                             <div class="diente" data-diente="diente{{$i}}" data-name="{{$dientes[$i-17]}}">
                                 <div class="dent{{$i}}">
-                                    <div class="trapecio-a"></div>
-                                    <div class="trapecio-b"></div>
+                                    <div class="trapecio-1"></div>
+                                    <div class="trapecio-3"></div>
                                     <div class="cuadrado"></div>
-                                    <div class="trapecio-i"></div>
-                                    <div class="trapecio-d"></div>
+                                    <div class="trapecio-4"></div>
+                                    <div class="trapecio-2"></div>
                                     <div class="cross" style="background-color: transparent"></div>
                                     <div class="cross2" style="background-color: transparent"></div>
                                     <div class="puente" style="background-color: transparent"></div>
@@ -119,8 +119,48 @@
         var nombre=($(this).data("name"));
         $('#myModal').modal('show');
         $('#name_diente').text(nombre);
-        $('#info').html(data[idc]);
+         var cont = modal_detalle(data[idc]);
+        $('#info').html(cont);
     });
+
+    function modal_detalle(data_dent) {
+        var comas = data_dent.split(',');
+        var ident;
+        var cadena = '<div class="row">' +
+            '<div class="col-lg-12"><div class="diente-m">\n' +
+            '<div class="cruz" style="height: 40px;margin-left: 14px"></div>\n' +
+            '<div class="cruz2" style="height: 40px;margin: -38px 0 0 13px"></div>\n' +
+            '<div class="cuadrado" style="margin-top: -27px; background-color: white; position: absolute"></div>\n' +
+            '<div class="num" style="margin: -42px 0 0 12px;">1</div>\n' +
+            '<div class="num" style="margin: -12px 0 0 21px;">2</div>\n' +
+            '<div class="num" style="margin: -24px 0 0 1px;">4</div>\n' +
+            '<div class="num" style="margin: -13px 0 0 11px;">3</div>\n' +
+            '<div class="num" style="margin: -33px 0 0 11px;position:absolute;">5</div>\n' +
+            '</div><br>';
+        cadena += '<ul>';
+        for(i=0; i<comas.length; i++){
+            var guion = comas[i].split('-');
+            if(guion.length>1){
+                var color;
+                (guion[0]=='r') ? color = 'red' : color = '#17a2b8';
+                cadena += '<li><div class="circulo" style="background: '+color+'"></div> ';
+                (guion[1]=='1') ? ident = '6' : ident = '4';
+                cadena += '<div style="margin: -19px 0 0 '+ident+'px; color: black;font-size: 11px" >'+guion[1]+'</div>';
+                cadena += '<div style="margin: -20px 0 0 25px;">'+guion[2]+'</div></li>';
+            }
+            if(guion[0]=='x'){
+                cadena += '<div class="cruz"></div>\n' +
+                    '<div class="cruz2"></div>\n' +
+                    '<div style="margin: -20px 0 0 25px;">Extracción</div>';
+            }
+            if(guion[0]=='p') {
+                cadena += '<div class="puente" style="height: 20px; margin: 0 0 0 5px;"></div>\n' +
+                    '<div style="margin: -20px 0 0 25px;">Puente</div>'
+            }
+        }
+        cadena = cadena + '</ul></div></div>';
+        return cadena;
+    }
 
     function graficar_dent(data_dent,num) {
         var comas = data_dent.split(','); //primer dato
@@ -128,12 +168,9 @@
             var guion = comas[i].split('-');
             if(guion.length>1){
                 var color;
-                if(guion[0]=='r')
-                    color = '#e94222';
-                else
-                    color = '#17a2b8';
+                (guion[0]=='r') ? color = 'red' : color = '#17a2b8';
 
-                if(guion[1]=='c')
+                if(guion[1]=='5')
                     $('.dent'+num+' .cuadrado').css('background-color',color);
 
                 //console.log(guion);
@@ -144,7 +181,7 @@
                 $('.dent'+num+' .cross2').css('background-color','black');
             }
             if(guion[0]=='p'){
-                $('.dent'+num+' .puente').css('background-color','#e94222');
+                $('.dent'+num+' .puente').css('background-color','red');
             }
 
         }
